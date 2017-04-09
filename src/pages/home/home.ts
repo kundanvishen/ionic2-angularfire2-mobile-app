6ba4365f7f1399dd977acbe8e6eb110ab97a2c77
Kundan Singh <kundan.singh979@gmail.com>
2017-04-09 23:24:28 +0530

    Created feeds page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+const data = [{
+  title: 'post1',
+  body: 'body text 1',
+  id: 0
+},{
+  title: 'post2',
+  body: 'body text 2',
+  id: 1
+},{
+  title: 'post3',
+  body: 'body text 3',
+  id: 2
+}];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  posts: any
   constructor(public navCtrl: NavController) {
-
+    this.posts = data;
   }
 
 }
